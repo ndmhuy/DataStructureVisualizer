57f90d9b2b03fbc0c5c2bb211aae405a630e84ba
SpheraXX <binbo242@gmail.com>
2026-03-08 12:16:07 +0700

    feat: initial setup

---
+++ DataStructureVisualizer/include/Controller/DataManager.h
@@ -0,0 +1,16 @@
+#ifndef DATAMANAGER_HPP
+#define DATAMANAGER_HPP
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+class DataManager
+{
+private:
+    std::vector<int> data;
+public:
+};
+
+#endif
---
+++ DataStructureVisualizer/src/Controller/DataManager.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <ctime>
+#include <cstdlib>
+
+#include "DataManager.h"
+
+
+
