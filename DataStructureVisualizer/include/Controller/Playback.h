29642a2ae765a85bf012e5e7323114ede3bfb267
Nguyen Dinh Minh Huy <ndmhuy2521@apcs.fitus.edu.vn>
2026-03-15 23:20:09 +0700

    feat: Implement Playback Controller to process the Timeline

---
+++ DataStructureVisualizer/include/Controller/Playback.h
@@ -0,0 +1,33 @@
+#ifndef PLAYBACK_H
+#define PLAYBACK_H
+
+#include "Model/Timeline.h"
+
+class Playback {
+    private:
+    Timeline currentTimeline;
+    bool isPlaying;
+    float speedMultiplier;
+    float timeAccumulator;
+    float baseStepDuration;
+
+    public:
+    Playback();
+
+    void update(float deltaTime);
+
+    void play();
+    void pause();
+    void stepForward();
+    void stepBackward();
+    void goToFinalStep();
+    void setSpeed(float multiplier);
+    void setBaseStepDuration(float seconds);
+
+    const Timeline& getTimeline() const;
+    void setTimeline(const Timeline& timeline);
+
+    bool isPlayingState() const;
+};
+
+#endif // PLAYBACK_H
