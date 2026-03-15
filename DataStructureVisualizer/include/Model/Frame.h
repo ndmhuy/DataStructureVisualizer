02530d3dc3a0f9f40591923498d4530332a56e02
Nguyen Dinh Minh Huy <ndmhuy2521@apcs.fitus.edu.vn>
2026-03-15 23:15:16 +0700

    feat: Define Frame to hold the current state and Timeline to hold a sequence of Frame

---
+++ DataStructureVisualizer/include/Model/Frame.h
@@ -0,0 +1,23 @@
+#ifndef FRAME_H
+#define FRAME_H
+
+#include <string>
+#include <vector>
+
+class Frame {
+    private:
+    std::vector<int> currentData;
+    std::vector<int> highlightIndices;
+    int codeLineId;
+    std::string message;
+
+    public:
+    Frame(const std::vector<int>& data, const std::vector<int>& highlight, int lineId, const std::string& msg);
+
+    const std::vector<int>& getCurrentData() const;
+    const std::vector<int>& getHighlightIndices() const;
+    int getCodeLineId() const;
+    const std::string& getMessage() const;
+};
+
+#endif // FRAME_H
