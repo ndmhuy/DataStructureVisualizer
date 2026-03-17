de71f41f6c5ab1dc138748765ed6c2b03cb4e181
Nguyen Dinh Minh Huy <ndmhuy2521@apcs.fitus.edu.vn>
2026-03-17 08:34:11 +0700

    fix: Fix encapsulation and syntax

--- DataStructureVisualizer/include/Model/Timeline.h
+++ DataStructureVisualizer/include/Model/Timeline.h
@@ -5,15 +5,24 @@
 #include "Frame.h"
 
 class Timeline {
-    public:
+    private:
     std::vector<Frame> frames;
     int currentFrameIndex;
 
+    public:
     Timeline();
     void addFrame(const Frame& frame);
-    void nextFrame();
     void clear();
+
+    void nextFrame();
+    void prevFrame();
+    void goToFrame(int index);
+    void goToFirstFrame();
+    void goToLastFrame();
+
+    bool isAtEnd() const;
     int getFrameCount() const;
+    int getCurrentFrameIndex() const;
     const Frame* getCurrentFrame() const;
 };
 
