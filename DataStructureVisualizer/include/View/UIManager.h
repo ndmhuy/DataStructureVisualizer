8ad0dcf845c3198f26a581697be03bd9f21e57cb
Nhu Khai <trannhukhai98438@gmail.com>
2026-03-22 15:34:01 +0700

    fix: split files

--- DataStructureVisualizer/include/View/UIManager.h
+++ DataStructureVisualizer/include/View/UIManager.h
@@ -3,11 +3,12 @@
 #include <SFML/System/Clock.hpp>
 #include "imgui.h"
 #include "imgui-SFML.h"
+#include "Button.h"
 
 class UIManager{
     private:
     //statics values
-
+        
     public:
 
     bool init(sf::RenderWindow&); //Initial set up for ImGui+SFML
@@ -19,39 +20,4 @@ class UIManager{
     void render(sf::RenderWindow&); //draw UI
 
     void shutdown(); //shutdown Imgui
-
-};
-
-class Button{
-    private:
-        //statics
-        bool isActive=true; //If the button is active (dim/light)
-        bool isPressed=false; //If the button is clicked
-        bool isHovered=false; // If the mouse is in the button
-
-        //image
-        sf::Texture texture;
-        sf::Sprite sprite;
-
-        //Color
-        sf::Color normal{255,255,255,255};
-        sf::Color hovered{200,200,200,255};
-        sf::Color pressed{150,150,150,255};
-        sf::Color notActive{150,150,150,100};
-    
-    public:
-
-    //create a button with image and fixed position and size
-    bool init(const std::string& , sf::Vector2f, sf::Vector2f);
-
-    //determine if the Button is active or not
-    void setActive(bool);
-
-    //Address if the button is clicked
-    bool handleEvent(const sf::RenderWindow&, const sf::Event& );
-
-    //draw
-    void render(sf::RenderWindow&);
-
-
 };
