2741e205845024661902b72a8b4665a7ea6385df
Nguyen Dinh Minh Huy <ndmhuy2521@apcs.fitus.edu.vn>
2026-04-02 14:03:44 +0700

    refactor: Integrate renderer/ui/theme into main loop

--- DataStructureVisualizer/include/Controller/AppEngine.h
+++ DataStructureVisualizer/include/Controller/AppEngine.h
@@ -3,19 +3,26 @@
 
 #include "Controller/Playback.h"
 #include "View/Window.h"
+#include "View/Theme.h"
+#include "View/Renderer.h"
+#include "View/UIManager.h"
 
 class AppEngine {
     private:
+    Window window;
+    bool shouldClose = false;
+    Theme theme;
+    Renderer renderer;
+    UIManager uiManager;
     Playback playbackController;
-    Window appWindow;
 
     public:
     AppEngine();
 
     void run();
 
-    void processInput();
-    void update(float deltaTime);
+    void processInput(const sf::Event& event);
+    void update(sf::Time deltaTime);
     void render();
 };
 
