c24e6f7c93f4ca742a9e7101fe22771489b0ec0f
Nguyen Dinh Minh Huy <ndmhuy2521@apcs.fitus.edu.vn>
2026-04-24 10:11:42 +0700

    feat: Introduce new Global Config and Position operators

--- DataStructureVisualizer/include/Model/Frame.h
+++ DataStructureVisualizer/include/Model/Frame.h
@@ -11,7 +11,6 @@ private:
     std::string message;
     IPayload* payload;
 
-<<<<<<< HEAD
 public:
     Frame(const LinkedListPayload& p, int lineId, const std::string& msg);
     Frame(const TreePayload& p, int lineId, const std::string& msg);
@@ -20,13 +19,6 @@ public:
     Frame(const SingleSourcePayload& p, int lineId, const std::string& msg);
     Frame(const AStarPayload& p, int lineId, const std::string& msg);
     Frame(const AllPairsPayload& p, int lineId, const std::string& msg);
-=======
-    public:
-    Frame(const std::vector<int>& arrData, const std::vector<size_t>& highlights, int lineId, const std::string& msg);
-    Frame(const std::vector<std::string>& strData, const std::vector<size_t>& highlights, int lineId, const std::string& msg);
-    Frame(const std::vector<size_t>& vertices, const std::vector<Edge>& edges, const std::vector<size_t>& highlightedNodes, const std::vector<Edge>& highlightedEdges, int lineId, const std::string& msg);
-    Frame(const std::vector<int>& arrData, const std::vector<size_t>& vertices, const std::vector<Edge>& edges, const std::vector<size_t>& highlightedNodes, const std::vector<Edge>& highlightedEdges, int lineId, const std::string& msg);
->>>>>>> 9f4a91b (feat: Implement GridGraph for grid representation, implement Dijkstra and Bellman-Ford (incomplete))
 
     // Rule of Three: destructor, copy constructor, assignment operator
     ~Frame();
--- DataStructureVisualizer/include/Utilities/Position.h
+++ DataStructureVisualizer/include/Utilities/Position.h
@@ -7,6 +7,42 @@ struct Position {
 
     Position() : x(0.0f), y(0.0f) {}
     Position(float x, float y) : x(x), y(y) {}
+
+    static float getDistance(const Position& a, const Position& b) {
+        float dx = a.x - b.x;
+        float dy = a.y - b.y;
+        return std::sqrt(dx * dx + dy * dy);
+    }
+
+    Position operator+(const Position& other) const {
+        return Position(x + other.x, y + other.y);
+    }
+
+    Position operator+=(const Position& other) {
+        x += other.x;
+        y += other.y;
+        return *this;
+    }
+
+    Position operator-(const Position& other) const {
+        return Position(x - other.x, y - other.y);
+    }
+
+    Position operator-=(const Position& other) {
+        x -= other.x;
+        y -= other.y;
+        return *this;
+    }
+
+    Position operator*(float scalar) const {
+        return Position(x * scalar, y * scalar);
+    }
+
+    Position operator*=(float scalar) {
+        x *= scalar;
+        y *= scalar;
+        return *this;
+    }
 };
 
 #endif // POSITION_H
